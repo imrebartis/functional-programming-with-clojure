f17e38b577e7995353830941cb6964ef3f1fdbda
Imre Bartis <imre.bartis@siili.com>
2017-11-01 16:11:29 +0200

    commands

--- mire/src/mire.clj
+++ mire/src/mire.clj
@@ -1,15 +1,20 @@
 #!/usr/bin/env clj
 
 (ns mire
+  (:use [mire commands])
   (:use [clojure.contrib server-socket duck-streams]))
 
 (def port (* 3 1111))
+(def prompt "> ")
 
 (defn mire-handle-client [in out]
   (binding [*in* (reader in)
             *out* (writer out)]
-    (loop []
-      (println (read-line))
-      (recur))))
+     (print prompt) (flush)
+    (loop [input (read-line)]
+      (println (execute input))
+      (print prompt)
+      (flush)
+      (recur (read-line)))))
 
 (def server (create-server port mire-handle-client))
