dc1ab63ef7dd72e56d25250db3e35b3bfaa6ef02
Imre Bartis <imre.bartis@siili.com>
2017-11-08 11:26:08 +0200

    other players + Wrap the whole body of move in a dosync since @current-room needs atomicity

--- mire/src/mire.clj
+++ mire/src/mire.clj
@@ -1,5 +1,7 @@
 #!/usr/bin/env clj
 
+(add-classpath (str "file://" (.getParent (java.io.File. *file*)) "/"))
+
 (ns mire
   (:use [mire commands rooms])
   (:use [clojure.contrib server-socket duck-streams]))
@@ -9,14 +11,21 @@
 
 (defn- mire-handle-client [in out]
   (binding [*in* (reader in)
-            *out* (writer out)
-            *current-room* (rooms :start)]
-    (println (look))
-    (print prompt) (flush)
-    (loop [input (read-line)]
-      (println (execute input))
-      (print prompt)
-      (flush)
-      (recur (read-line)))))
+            *out* (writer out)]
+
+    ;; We have to nest this in another binding call instead of using
+    ;; the one above so *in* and *out* will be bound to the socket
+    (print "\nWhat is your name? ") (flush)
+    (binding [player-name (read-line)
+              *current-room* (ref (rooms :start))]
+      (dosync (alter (:inhabitants @*current-room*) conj player-name))
+
+      (println (look)) (print prompt) (flush)
+
+      (loop [input (read-line)]
+        (println (execute input))
+        (print prompt)
+        (flush)
+        (recur (read-line))))))
 
 (def server (create-server port mire-handle-client))
--- mire/src/mire/commands.clj
+++ mire/src/mire/commands.clj
@@ -1,24 +1,27 @@
 (ns mire.commands
-  (:use [mire rooms util])
+  (:use [mire rooms])
   (:use [clojure.contrib str-utils seq-utils]))
 
 ;; Command functions
 
 (defn look "Get a description of the surrounding environs and its contents."
   []
-  (str (:desc *current-room*)
-       "\nExits: " (keys (:exits *current-room*))
+  (str (:desc @*current-room*)
+       "\nExits: " (keys (:exits @*current-room*))
        ".\n"))
 
 (defn move
   "\"♬ We gotta get out of this place... ♪\" Give a direction."
   [direction]
-  (let [target-name ((:exits *current-room*) (keyword direction))
-        target (rooms target-name)]
-    (if target
-      (do (set! *current-room* target)
-          (look))
-      "You can't go that way.")))
+  (dosync
+   (let [target-name ((:exits @*current-room*) (keyword direction))
+         target (rooms target-name)]
+     (if target
+       (do (alter (:inhabitants @*current-room*) disj player-name)
+           (alter (:inhabitants target) conj player-name)
+           (ref-set *current-room* target)
+           (look))
+       "You can't go that way."))))
 
 ;; Command data
 
