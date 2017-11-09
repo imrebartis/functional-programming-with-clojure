5c81b5dd3600879a4568ee361ef683bd50d0e5d2
Imre Bartis <imre.bartis@siili.com>
2017-11-09 10:18:21 +0200

    items

--- mire/src/mire/commands.clj
+++ mire/src/mire/commands.clj
@@ -1,5 +1,5 @@
 (ns mire.commands
-  (:use [mire rooms])
+  (:use [mire rooms player util])
   (:use [clojure.contrib str-utils seq-utils]))
 
 ;; Command functions
@@ -7,8 +7,9 @@
 (defn look "Get a description of the surrounding environs and its contents."
   []
   (str (:desc @*current-room*)
-       "\nExits: " (keys @(:exits @*current-room*))
-       ".\n"))
+       "\nExits: " (keys @(:exits @*current-room*)) "\n"
+       (str-join "\n" (map #(str "There is " % " here.\n")
+                           @(:items @*current-room*)))))
 
 (defn move
   "\"♬ We gotta get out of this place... ♪\" Give a direction."
@@ -17,11 +18,41 @@
    (let [target-name ((:exits @*current-room*) (keyword direction))
          target (rooms target-name)]
      (if target
-       (do (alter (:inhabitants @*current-room*) disj player-name)
-           (alter (:inhabitants target) conj player-name)
-           (ref-set *current-room* target)
-           (look))
+       (do
+         (move-between-refs *player-name*
+                            (:inhabitants @*current-room*)
+                            (:inhabitants target))
+         (ref-set *current-room* target)
+         (look))
        "You can't go that way."))))
+
+(defn grab
+  "Pick something up."
+  [thing]
+  (dosync
+   (if (room-contains? @*current-room* thing)
+     (do (move-between-refs (keyword thing)
+                            (:items @*current-room*)
+                            *inventory*)
+         (str "You picked up the " thing "."))
+     (str "There isn't any " thing " here."))))
+
+(defn discard
+  "Put something down that you're carrying."
+  [thing]
+  (dosync
+   (if (carrying? thing)
+     (do (move-between-refs (keyword thing)
+                            *inventory*
+                            (:items @*current-room*))
+         (str "You dropped the " thing "."))
+     (str "You're not carrying a " thing "."))))
+
+(defn inventory
+  "See what you've got."
+  []
+  (str "You are carrying:\n"
+       (str-join "\n"  @*inventory*)))
 
 ;; Command data
 
@@ -30,6 +61,9 @@
                "south" (fn [] (move :south)),
                "east" (fn [] (move :east)),
                "west" (fn [] (move :west)),
+               "grab" grab
+               "discard" discard
+               "inventory" inventory
                "look" look})
 
 ;; Command handling
@@ -40,4 +74,4 @@
   (let [input-words (re-split #"\s+" input)
         command (first input-words)
         args (rest input-words)]
-    (apply (commands command) args)))
+     (apply (commands command) args)))
